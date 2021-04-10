ffc97713ac641079fa0657c99af7c48ed05e5626
root <wachowsky.artur@gmail.com>
2021-04-10 15:25:14 -0400

    Added distance class separation

---
+++ distance.py
@@ -0,0 +1,28 @@
+from math import radians, cos, sin, asin, sqrt
+
+#https://github.com/mapado/haversine/blob/master/tests/test_haversine.py
+class Distance:
+
+    def __init__(self, earth, office):
+        self.earth = earth
+        self.office = office
+    #Returns distance in KMs
+    def get_km(self,la,lo):
+
+        # unpack latitude/longitude
+        lat1, lng1 = self.office
+        lat2 = float(la)
+        lng2 = float(lo)
+
+        # convert all latitudes/longitudes from decimal degrees to radians
+        lat1 = radians(lat1)
+        lng1 = radians(lng1)
+        lat2 = radians(lat2)
+        lng2 = radians(lng2)
+
+        # calculate haversine
+        lat = lat2 - lat1
+        lng = lng2 - lng1
+        d = sin(lat * 0.5) ** 2 + cos(lat1) * cos(lat2) * sin(lng * 0.5) ** 2
+
+        return int(2 * self.earth  * asin(sqrt(d)))
